d8d9f39aeb7a6ff007b71212cf0562958a0829bc
Alex Chavez <chavezzz@gmail.com>
2018-10-20 18:18:01 +0000

    adding idea project files and emptying cart via ajax

---
+++ app/views/carts/destroy.js.coffee
@@ -0,0 +1,7 @@
+cart = document.getElementById("cart")
+cart.innerHTML = ""
+
+notice = document.getElementById("notice")
+if notice
+  notice.innerHTML = "Your cart was emptied."
+#TODO:alex create new notice element.
--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -47,4 +47,16 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to store_index_url
   end
+
+  test 'should destroy cart via ajax' do
+    post line_items_url, params: { product_id: products(:ruby).id }, xhr: true
+    @cart = Cart.find(session[:cart_id])
+    assert_difference('Cart.count', -1) do
+      delete cart_url(@cart)
+    end
+
+    puts @response.body
+    assert_response :success
+
+  end
 end
